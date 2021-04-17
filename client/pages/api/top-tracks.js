f7cfa32db75ed150871e40155bc04efa3356ea02
Thomas Gormley <thomasgormley415@gmail.com>
2021-04-18 00:08:36 +0100

    env test

--- client/pages/api/top-tracks.js
+++ client/pages/api/top-tracks.js
@@ -1,14 +1,18 @@
 import { getTopTracks } from '../../lib/spotify';
+import config from '../../lib/config';
+
+const { client_id } = config.spotify;
 
 export default async (_, res) => {
     const response = await getTopTracks();
     const { items } = await response.json();
-
+    console.log(client_id);
     const tracks = items.slice(0, 10).map((track) => ({
         artist: track.artists.map((_artist) => _artist.name).join(', '),
         songUrl: track.external_urls.spotify,
         title: track.name,
         images: track.album.images,
+        client_id,
     }));
 
     return res.status(200).json({ tracks });
